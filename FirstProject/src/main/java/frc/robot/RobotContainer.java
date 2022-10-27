c0c85ceef4abc216b1ff24dd5493c77be8887522
MC Ride <hackbots3414@gmail.com>
2022-10-27 18:59:38 -0400

    Clean up RobotContainer.java and setup auton and teleop command selection
    I cleaned up RobotContainer.java, which removed the default
    select command, the loop command, the enum, and cleaned up the imports.
    I also setup the default teleop and auton commands.

--- FirstProject/src/main/java/frc/robot/RobotContainer.java
+++ FirstProject/src/main/java/frc/robot/RobotContainer.java
@@ -8,73 +8,29 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.SelectCommand;
-import java.util.Map;
-import frc.robot.commands.SuperLooper;
+import frc.robot.commands.DefaultTeleopCommand;
+import frc.robot.commands.DriveStrait;
 import frc.robot.subsystem.DriveTrain;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  private  static RobotContainer me = new RobotContainer();
+  private static RobotContainer me = new RobotContainer();
   private XboxController controller = new XboxController(0);
   private DriveTrain drvTrain = new DriveTrain();
- 
-
-  // The enum used as keys for selecting the command to run.
-  private enum CommandSelector {
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    EMPTY,
-    INVALID
-  }
-
-  // An example selector method for the selectcommand.  Returns the selector that will select
-  // which command to run.  Can base this choice on logical conditions evaluated at runtime.
-  private CommandSelector select() {
-    double selection = SmartDashboard.getNumber(Constants.AUTON_CMD_NAME, 0);
-    if (selection == 0.00) {
-      return CommandSelector.EMPTY;
-    } else if (selection == 1.00) {
-      return CommandSelector.ONE;
-    } else if (selection == 2.00) {
-      return CommandSelector.TWO;
-    } else if (selection == 3.00) {
-      return CommandSelector.THREE; 
-    } else if (selection == 4.00) {
-      return CommandSelector.FOUR;
-    }
-    // anything else is conidered invalid...
-    return CommandSelector.INVALID;
-
-  }
-
-  // An example selectcommand.  Will select from the three commands based on the value returned
-  // by the selector method at runtime.  Note that selectcommand works on Object(), so the
-  // selector does not have to be an enum; it could be any desired type (string, integer,
-  // boolean, double...)
-  private final Command m_exampleSelectCommand =
-      new SelectCommand(
-          // Maps selector values to commands
-          Map.ofEntries(
-              Map.entry(CommandSelector.ONE, new PrintCommand("Command one was selected!")),
-              Map.entry(CommandSelector.TWO, new PrintCommand("Command two was selected!")),
-              Map.entry(CommandSelector.THREE, new PrintCommand("Command three was selected!")),
-              Map.entry(CommandSelector.FOUR, new SuperLooper()),
-              Map.entry(CommandSelector.EMPTY, new PrintCommand("No command has been selected yet. Perhaps put tomething there?")),
-              Map.entry(CommandSelector.INVALID, new PrintCommand("The option you selected is quite invalid."))),
-          this::select);
+  private DriveStrait auton_command = new DriveStrait(drvTrain, 1, 0.1);
 
   private RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+    drvTrain.setDefaultCommand(new DefaultTeleopCommand(drvTrain));
     SmartDashboard.putNumber(Constants.AUTON_CMD_NAME, 0);
   }
 
@@ -87,12 +43,15 @@ public class RobotContainer {
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling passing it to a
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
+   * passing it to a
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -100,7 +59,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return m_exampleSelectCommand;
+    return auton_command;
   }
 
   public XboxController getGamePad() {
