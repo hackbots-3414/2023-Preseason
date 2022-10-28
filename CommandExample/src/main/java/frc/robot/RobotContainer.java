3ba666f4d5daa77228646031fd82da50e6ced871
Nick <nicholaswinder@gmail.com>
2022-10-27 20:03:33 -0400

    added default command

--- CommandExample/src/main/java/frc/robot/RobotContainer.java
+++ CommandExample/src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SelectCommand;
+import frc.robot.commands.DefaultTeleopCommand;
 import frc.robot.commands.DriveStraight;
 import frc.robot.commands.NWLoop;
 import frc.robot.subsystems.Drivetrain;
@@ -70,6 +71,7 @@ public class RobotContainer {
     // Configure the button bindings
     configureButtonBindings();
     SmartDashboard.putNumber(Constants.AUTON_COMMAND_NUMBER, 4);
+    drvtrain.setDefaultCommand(new DefaultTeleopCommand(drvtrain));
   }
 
   public static RobotContainer getInstance() {
