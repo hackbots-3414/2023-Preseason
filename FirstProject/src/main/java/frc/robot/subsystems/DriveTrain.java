175c48a203dfea5c090aba76f4e5cc77f04030f1
HeidiG2007 <78167256+HeidiG2007@users.noreply.github.com>
2022-11-03 18:53:51 -0400

    Change variable rear to back to make it easier to remember

--- FirstProject/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ FirstProject/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -12,26 +12,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DriveTrain extends SubsystemBase {
   private WPI_TalonFX lfMotor = new WPI_TalonFX(Constants.LF_MOTOR);  //lf = left front
   private WPI_TalonFX rfMotor = new WPI_TalonFX(Constants.RF_MOTOR);  //rf = right front
-  private WPI_TalonFX lrMotor = new WPI_TalonFX(Constants.LB_MOTOR);  //lr = left rear
-  private WPI_TalonFX rrMotor = new WPI_TalonFX(Constants.RB_MOTOR);
+  private WPI_TalonFX lbMotor = new WPI_TalonFX(Constants.LB_MOTOR);  //lb = left back
+  private WPI_TalonFX rbMotor = new WPI_TalonFX(Constants.RB_MOTOR);  //rb = right back
   private DifferentialDrive dfDrive = new DifferentialDrive(lfMotor, rfMotor);
 
   /** Creates a new DriveTrain. */
   public DriveTrain() {
 
     lfMotor.setSafetyEnabled(true);
-    lrMotor.setSafetyEnabled(true);
+    lbMotor.setSafetyEnabled(true);
     rfMotor.setSafetyEnabled(true);
-    rrMotor.setSafetyEnabled(true);
+    rbMotor.setSafetyEnabled(true);
 
     //We tell the lr and rr motors to follow the front ones.
-    lrMotor.follow(lfMotor);
-    rrMotor.follow(rfMotor);
+    lbMotor.follow(lfMotor);
+    rbMotor.follow(rfMotor);
   
     //Following Hackbots convention, we invert the right motors.
     rfMotor.setInverted(true);
     //Invert is specific to the motor, so even though we told the rr motor to follow rf, we still need to invert it.
-    rrMotor.setInverted(true);
+    rbMotor.setInverted(true);
 
   }
 
