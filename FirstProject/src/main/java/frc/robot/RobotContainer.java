8de22932f29cd9922515938f918a4a0ff75e525b
HeidiG2007 <78167256+HeidiG2007@users.noreply.github.com>
2022-11-07 18:59:39 -0500

    teleop logger & comment driveTurn import container

--- FirstProject/src/main/java/frc/robot/RobotContainer.java
+++ FirstProject/src/main/java/frc/robot/RobotContainer.java
@@ -8,13 +8,13 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-//import edu.wpi.first.wpilibj2.command.PrintCommand;
-//import edu.wpi.first.wpilibj2.command.SelectCommand;
-//import frc.robot.commands.HeidiLoopyCommand;
+/*import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SelectCommand;
+import frc.robot.commands.HeidiLoopyCommand;*/
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.commands.DefaultTeleopCommand;
 import frc.robot.commands.DriveStraight;
-import frc.robot.commands.DriveTurn;
+//import frc.robot.commands.DriveTurn;
 
 //import java.util.Map;
 
--- FirstProject/src/main/java/frc/robot/commands/DefaultTeleopCommand.java
+++ FirstProject/src/main/java/frc/robot/commands/DefaultTeleopCommand.java
@@ -4,11 +4,14 @@
 
 package frc.robot.commands;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveTrain;
 
 public class DefaultTeleopCommand extends CommandBase {
+  private static final Logger LOG = LoggerFactory.getLogger(DefaultTeleopCommand.class);
   private DriveTrain driveTrain;
 
   public DefaultTeleopCommand(DriveTrain drvTrain) {
@@ -27,6 +30,7 @@ public class DefaultTeleopCommand extends CommandBase {
   public void execute() {
     double xSpeed = RobotContainer.getInstance().getGamePad().getLeftY();
     double zRotation = RobotContainer.getInstance().getGamePad().getRightX();
+    LOG.trace("execute(): xSpeed: {}, zRotaion{}", xSpeed, zRotation);
     driveTrain.drive(xSpeed, zRotation);
   }
 
