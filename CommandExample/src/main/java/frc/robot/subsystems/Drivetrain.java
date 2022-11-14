369bc21267fccfd1920a570cdce0cd1d2bded318
Nick <nicholaswinder@gmail.com>
2022-11-14 17:51:00 -0500

    fixed not sending often enough for motors

--- CommandExample/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ CommandExample/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -32,6 +32,8 @@ public class Drivetrain extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    motorBR.feed();
+    motorBL.feed();
   }
 
   public void drive(double speed, double rot) {
