debe0a85d733dd5bb962c007aa4196e2b74c825b
HeidiG2007 <78167256+HeidiG2007@users.noreply.github.com>
2022-12-08 18:20:59 -0500

    Get rid of SmartDashboard

--- FirstProject/src/main/java/frc/robot/RobotContainer.java
+++ FirstProject/src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 /*import edu.wpi.first.wpilibj2.command.PrintCommand;
@@ -44,7 +44,7 @@ public class RobotContainer {
     // Configure the button bindings
     configureButtonBindings();
     drvTrain.setDefaultCommand(new DefaultTeleopCommand(drvTrain));
-    SmartDashboard.putNumber(Constants.AUTON_CMD_NAME, 0);
+    //SmartDashboard.putNumber(Constants.AUTON_CMD_NAME, 0);
   }
   
   public static RobotContainer getInstance() {
