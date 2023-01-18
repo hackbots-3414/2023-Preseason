7addd068d2f9318d003b412bca3a8711288276c6
Vijay N <vijaynannapuraju@gmail.com>
2023-01-18 18:13:12 -0500

    lmao my build was successful

--- PhotonVisionOne-Imported/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ PhotonVisionOne-Imported/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,11 +4,50 @@
 
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 /** Add your docs here. */
-public class DriveTrain extends SubsystemBase {}
+public class DriveTrain extends SubsystemBase {
+    private WPI_TalonFX motorRightFront;
+    private WPI_TalonFX motorRightBack;
+    private WPI_TalonFX motorLeftFront;
+    private WPI_TalonFX motorLeftBack;
+    private DifferentialDrive diffDrive;
+
+    public DriveTrain() {
+        motorRightFront = new WPI_TalonFX(10);
+        motorRightBack = new WPI_TalonFX(11);
+        motorLeftFront = new WPI_TalonFX(13);
+        motorLeftBack = new WPI_TalonFX(14);
+
+        motorRightFront.setInverted(true);
+        motorRightBack.setInverted(true);
+
+        motorLeftBack.follow(motorLeftFront);
+        motorRightBack.follow(motorRightFront);
+
+        diffDrive = new DifferentialDrive(motorLeftFront, motorRightFront);
+    }
+    public void resetEncoders(){
+    motorLeftBack.getSelectedSensorPosition(0);
+    }
+
+    public double getDistance(){
+        return motorLeftBack.getSelectedSensorPosition();
+    }
+    public void drive(double xSpeed , double zRotation){
+        diffDrive.arcadeDrive(xSpeed, zRotation);
+    }
+    @Override
+    public void periodic(){
+    }
+}
 
 
