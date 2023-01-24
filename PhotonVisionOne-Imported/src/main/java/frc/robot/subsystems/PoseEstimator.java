d2fd7b95cb1dcdee808b478eeee295766136a046
HeidiG2007 <78167256+HeidiG2007@users.noreply.github.com>
2023-01-23 19:23:24 -0500

    Create PoseEstimator

--- PhotonVisionOne-Imported/src/main/java/frc/robot/subsystems/PoseEstimator.java
+++ PhotonVisionOne-Imported/src/main/java/frc/robot/subsystems/PoseEstimator.java
@@ -4,53 +4,16 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.AnalogGyro;
-import frc.robot.Constants;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import org.photonvision.PhotonCamera;
-import org.photonvision.SimVisionSystem;
+/** Add your docs here. */
+public class PoseEstimator extends SubsystemBase{
 
-public class PoseEstimator {
-    private PhotonCamera cam = new PhotonCamera(Constants.kCamName);
+    private Camera camera = new Camera();
 
-    public void update(double leftDist, double rightDist) {
-    }m_poseEstimator.update<gyro.getRotation2d,leftDist,rightDist>;
-
-    res=cam.getLatestResult(); {
-    if(res.hasTargets())}
-
-    {
-        Object res;
-        var imageCaptureTime = (res).getTimestampSeconds();
-        var camToTargetTrans = res.getBestTarget().getBestCameraToTarget();
-        var camPose = Constants.kFarTargetPose.transformBy(camToTargetTrans.inverse());
-        DifferentialDrivePoseEstimator m_poseEstimator;
-        m_poseEstimator.addVisionMeasurement(
-                camPose.transformBy(Constants.kCameraToRobot).toPose2d(), imageCaptureTime);
-
-        double camDiagFOV = 75.0; // degrees
-        double camPitch = 15.0; // degrees
-        double camHeightOffGround = 0.85; // meters
-        double maxLEDRange = 20; // meters
-        int camResolutionWidth = 640; // pixels
-        int camResolutionHeight = 480; // pixels
-        double minTargetArea = 10; // square pixels
-
-        SimVisionSystem simVision = new SimVisionSystem(
-                Constants.kCamName,
-                camDiagFOV,
-                Constants.kCameraToRobot,
-                maxLEDRange,
-                camResolutionWidth,
-                camResolutionHeight,
-                minTargetArea);
+    public Transform3d getPose(int tagid) {
+        Transform3d id = camera.getTransform3d(tagid);
+        return id;   
     }
-
-    public void DrivetrainSim(){
-        SimVisionSystem simVision;
-        simVision.addSimVisionTarget(Constants.kFarTarget);
-    }
-
-}}
+}
