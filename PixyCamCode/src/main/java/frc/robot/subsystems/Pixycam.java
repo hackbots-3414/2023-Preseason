e8a6c3ee77ba638d8821dc19d164d8b6fee7c0cf
julia_i <julia.d.ionescu@gmail.com>
2023-02-13 20:21:18 -0500

    Pixycam

--- PixyCamCode/src/main/java/frc/robot/subsystems/Pixycam.java
+++ PixyCamCode/src/main/java/frc/robot/subsystems/Pixycam.java
@@ -25,7 +25,7 @@ public class Pixycam extends SubsystemBase {
 		// Gets the number of "blocks", identified targets, that match signature 1 on the Pixy2,
 		// does not wait for new data if none is available,
 		// and limits the number of returned blocks to 25, for a slight increase in efficiency
-		int blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25);
+		int blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1 | Pixy2CCC.CCC_SIG2, 4);
 		System.out.println("Found " + blockCount + " blocks!"); // Reports number of blocks found
 		if (blockCount <= 0) {
 			return null; // If blocks were not found, stop processing
