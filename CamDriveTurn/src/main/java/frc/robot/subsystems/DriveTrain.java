1b6f39cb3a664af6b1bc36b97f5b0747675ecb8b
julia_i <julia.d.ionescu@gmail.com>
2023-04-10 19:52:21 -0400

    NotDone

--- CamDriveTurn/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ CamDriveTurn/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -9,55 +9,59 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.math.geometry.Quaternion;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 
 public class DriveTrain extends SubsystemBase {
-  private WPI_TalonFX motor1 = new WPI_TalonFX(1);
-  private WPI_TalonFX motor2 = new WPI_TalonFX(2);
-  private WPI_TalonFX motor3 = new WPI_TalonFX(3);
-  private WPI_TalonFX motor4 = new WPI_TalonFX(4);
-  private AHRS navX = new AHRS();
+  private WPI_TalonFX rightFront = new WPI_TalonFX(1);
+  private WPI_TalonFX leftFront = new WPI_TalonFX(2);
+  private WPI_TalonFX rightBack = new WPI_TalonFX(3);
+  private WPI_TalonFX leftBack = new WPI_TalonFX(4);
+  private AHRS ahrs = new AHRS(Port.kMXP);
+  
+  private final DifferentialDriveOdometry rotation;
 
   private DifferentialDrive drive;
   /** Creates a new DriveTrain. */
   public DriveTrain() {
-    motor3.follow(motor1);
-    motor4.follow(motor2);
-    motor1.setInverted(TalonFXInvertType.Clockwise);
-    motor3.setInverted(TalonFXInvertType.Clockwise);
-    motor2.setInverted(TalonFXInvertType.CounterClockwise);
-    motor4.setInverted(TalonFXInvertType.CounterClockwise);
+    rightBack.follow(rightFront);
+    leftBack.follow(leftFront);
+    rightFront.setInverted(TalonFXInvertType.Clockwise);
+    rightBack.setInverted(TalonFXInvertType.Clockwise);
+    leftFront.setInverted(TalonFXInvertType.CounterClockwise);
+    leftBack.setInverted(TalonFXInvertType.CounterClockwise);
 
-    drive = new DifferentialDrive(motor1, motor2);
+    drive = new DifferentialDrive(rightFront, leftFront);
 
+    rotation = new DifferentialDriveOdometry(ahrs.getRotation2d(), (leftFront.getSelectedSensorPosition() + leftBack.getSelectedSensorPosition()), (rightFront.getSelectedSensorPosition() + rightBack.getSelectedSensorPosition()));
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    motor1.feed();
-    motor2.feed();
-    motor3.feed();
-    motor4.feed();
+    rightFront.feed();
+    leftFront.feed();
+    rightBack.feed();
+    leftBack.feed();
 
+    rotation.update(ahrs.getRotation2d(), leftFront.getSelectedSensorPosition(), rightFront.getSelectedSensorPosition());
   }
   public void drive(double xSpeed, double zRotation) {
 
-    drive.arcadeDrive(xSpeed, zRotation);
-    if (navX.getQuaternionY() >= 90);
-      return;
-    if (navX.getQuaternionY() < 90);
-      
-
+    drive.arcadeDrive(xSpeed, zRotation);  
   }
   public void zeroNavX()
   {
-    navX.reset();
+    ahrs.reset();
   }
   public double getHeading()
   {
-    return navX.getQuaternionY();
+    return ahrs.getQuaternionY();
   }
 }
